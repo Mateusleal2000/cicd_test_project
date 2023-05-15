ac67cd93ed281d309688803815306b7e41922236
matlg <47640162+Mateusleal2000@users.noreply.github.com>
2023-05-15 20:56:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     logger.info(f"Token value: {SOME_SECRET}")
 
     r = requests.get(
-        'https://weather.talkpython.fm/api/weather/?city=Fortaleza&country=BR')
+        'https://weather.talkpython.fm/api/weather/?city=Brasilia&country=BR')
     if r.status_code == 200:
         data = r.json()
         temperature = data["forecast"]["temp"]
